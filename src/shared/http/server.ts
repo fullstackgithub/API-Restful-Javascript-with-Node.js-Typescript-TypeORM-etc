3e2b1e253dd3bfefa6d025e0ea7d001f2714a384
Wanderson Lima <fullstackgithub@gmail.com>
2022-05-15 20:44:15 +0100

    Creation and configuration of server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,36 @@
-console.log("Done!")
+import 'reflect-metadatayarn'
+import { NextFunction, Request, Response } from 'express';
+import express from 'express';
+import cors from 'cors';
+import routes from './routes'
+import AppError from './errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use((
+  error: Error, 
+  req: Request, 
+  res: Response, 
+  next: NextFunction,) => {
+    if(error instanceof AppError){
+      return res.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+        });
+    }
+
+    return res.status(500).json({
+      status: 'error', 
+      message: 'Internal server error'
+    });
+  } 
+);
+
+app.listen(3333, () => {
+  console.log('Server running on port 3333')
+});
